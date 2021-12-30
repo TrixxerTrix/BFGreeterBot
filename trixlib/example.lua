ccefdff1c879620194362d3c6439e38c16c31b3c
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2021-12-30 11:56:02 +0000

    Create example.lua

---
+++ trixlib/example.lua
@@ -0,0 +1,8 @@
+-----------------------------------------------------------------------------------------------------------------------------
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixxerTrix/become-fumo-stuff/main/trixlib/versions/0.5.lua"))()
+-----------------------------------------------------------------------------------------------------------------------------
+
+local utils = shared.t_utils
+utils.saveanim(workspace.PlayArea.TheDog.WhatDaDogDoin.InuAwake)
+
+utils.getanim(1):Play()
