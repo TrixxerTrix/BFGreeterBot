6495227567bdee1d4f8b5a8c0bbf2b7addefc89d
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2021-12-30 11:32:29 +0000

    Update loader.lua

--- trixlib/loader.lua
+++ trixlib/loader.lua
@@ -1,3 +1,3 @@
 -----------------------------------------------------------------------------------------------------------------------------
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixxerTrix/become-fumo-stuff/main/trixlib/versions/0.45.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixxerTrix/become-fumo-stuff/main/trixlib/versions/0.45-BUGFIX.lua"))()
 -----------------------------------------------------------------------------------------------------------------------------
